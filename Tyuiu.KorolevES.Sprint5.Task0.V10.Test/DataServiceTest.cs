c969ea163342855bdccf963f93ef035d27543ac0
Егор Королев <Tyuiu.KorolevES@outlook.com>
2024-11-24 16:24:46 +0500

    Task0

--- Tyuiu.KorolevES.Sprint5.Task0.V10.Test/DataServiceTest.cs
+++ Tyuiu.KorolevES.Sprint5.Task0.V10.Test/DataServiceTest.cs
@@ -7,8 +7,7 @@ namespace Tyuiu.KorolevES.Sprint5.Task0.V10.Test
         [TestMethod]
         public void TestMethod1()
         {
-            DataService dataService = new DataService();
-            string path = dataService.SaveToFileTextData(1);
+            string path = @"C:\Users\Admin\AppData\Local\Temp\OutPutFileTask0.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
