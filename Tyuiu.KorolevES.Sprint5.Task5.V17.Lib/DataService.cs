c99a9368b94a99edd0845d5d0f58e1719fa0f1ec
Егор Королев <Tyuiu.KorolevES@outlook.com>
2024-11-24 19:23:14 +0500

    Task5

--- Tyuiu.KorolevES.Sprint5.Task5.V17.Lib/DataService.cs
+++ Tyuiu.KorolevES.Sprint5.Task5.V17.Lib/DataService.cs
@@ -20,45 +20,14 @@ namespace Tyuiu.KorolevES.Sprint5.Task5.V17.Lib
                     for (int i = 0; i < array.Length; i++)
                     {
                         double a = Convert.ToDouble(array[i]);
-                        if ((a > 1) && (Math.Round(a) == a))
+                        if (a > 0)
                         {
-                            max =Math.Max(max, Convert.ToInt32(a));
-                        }
-                    }
-                    bool[] prost = new bool[max+1];
-                    prost[0] = false;
-                    prost[1] = false;
-                    for (int i = 2; i < prost.Length; i++)
-                    {
-                        int b = 0;
-                        for (int j = 2; j < i; j++)
-                        {
-                            if (i % j == 0)
-                            {
-                                b = 1;
-                                break;
-                            }
-                        }
-                        if (b == 0)
-                        {
-                            prost[i] = true;
-                        }
-                        else
-                        {
-                            prost[i] = false;
-                        }
-                    }
-                    for (int i = 0;i < array.Length; i++)
-                    {
-                        double a = Convert.ToDouble(array [i]);
-                        if ((a > 1) && (Math.Round(a, 0) == a) && (prost[Convert.ToInt32(a)] = true))
-                        {
-                            res += a;
+                            res+=a;
                         }
                     }
                 }
             }
-            return res;
+            return Math.Round(res,3);
         }
     }
 }
