400c6255aa6398f9ea02f9143487fba2fc67f801
Егор Королев <Tyuiu.KorolevES@outlook.com>
2024-11-26 16:50:07 +0500

    Task5

--- Tyuiu.KorolevES.Sprint5.Task5.V17.Lib/DataService.cs
+++ Tyuiu.KorolevES.Sprint5.Task5.V17.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.KorolevES.Sprint5.Task5.V17.Lib
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
-                while ((line = reader.ReadLine())!= null)
+                while ((line = reader.ReadLine()) != null)
                 {
                     line = line.Replace(".", ",");
                     array = line.Split(" ");
@@ -20,14 +20,45 @@ namespace Tyuiu.KorolevES.Sprint5.Task5.V17.Lib
                     for (int i = 0; i < array.Length; i++)
                     {
                         double a = Convert.ToDouble(array[i]);
-                        if (a > 0)
+                        if ((a > 1) && (Math.Round(a) == a))
                         {
-                            res+=a;
+                            max = Math.Max(max, Convert.ToInt32(a));
+                        }
+                    }
+                    bool[] prost = new bool[max + 1];
+                    prost[0] = false;
+                    prost[1] = false;
+                    for (int i = 2; i < prost.Length; i++)
+                    {
+                        int b = 0;
+                        for (int j = 2; j < i; j++)
+                        {
+                            if (i % j == 0)
+                            {
+                                b = 1;
+                                break;
+                            }
+                        }
+                        if (b == 0)
+                        {
+                            prost[i] = true;
+                        }
+                        else
+                        {
+                            prost[i] = false;
+                        }
+                    }
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        double a = Convert.ToDouble(array[i]);
+                        if ((a > 1) && (Math.Round(a, 0) == a) && (prost[Convert.ToInt32(a)] = true))
+                        {
+                            res += a;
                         }
                     }
                 }
             }
-            return Math.Round(res,3);
+            return res;
         }
     }
 }
